73a8586c161b8b19cb97c0ef8f9dcf75eb8fe945
yashjawale <yash25.j@gmail.com>
2025-08-11 21:12:51 +0530

    feat: add Starlight integration

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import starlight from '@astrojs/starlight';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [starlight({
+      title: 'My delightful docs site',
+    })]
+});
