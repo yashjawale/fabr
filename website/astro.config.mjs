02c1e4e1147953b96e20ef2d004a1ef7fab55f7c
yashjawale <yash25.j@gmail.com>
2025-08-13 10:49:53 +0530

    feat: tailiwind theme based on styleguide

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,16 +1,29 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import starlight from '@astrojs/starlight';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [starlight({
-      title: 'My delightful docs site',
-    })],
-
+    title: 'Fabr',
+    description: 'Project Template Generator',
+    customCss: [
+      './src/styles/global.css',
+    ],
+    expressiveCode: {
+      themes: ['github-light', 'github-dark'],
+    },
+    sidebar: [
+      {
+        label: 'Documentation',
+        items: [
+          { label: 'Getting Started', slug: 'docs' },
+          { label: 'Style Guide', slug: 'style-guide' },
+        ],
+      },
+    ],
+  })],
   vite: {
     plugins: [tailwindcss()]
   }
