6871b582b630b3581ad677e0b7978c39622e5c1f
yashjawale <yash25.j@gmail.com>
2025-08-15 22:18:17 +0530

    fix: update links in Navbar and FrameworksSection, and adjust site configuration in astro.config.mjs

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -5,31 +5,33 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: 'Fabr',
-    description: 'Project Template Generator',
-	logo: {
-		dark: './src/assets/fabr-docs-dark.svg',
-		light: './src/assets/fabr-docs-light.svg',
-		alt: 'fabr Docs',
-		replacesTitle: true,
-	},
-    customCss: [
-      './src/styles/starlight-theme.css',
-    ],
-    expressiveCode: {
-      themes: ['github-light', 'github-dark'],
-    },
-    sidebar: [
-      {
-        label: 'General',
-        items: [
-          { label: 'Getting Started', slug: 'docs' },
-        ],
-      },
-    ],
-  })],
-  vite: {
-    plugins: [tailwindcss()]
-  }
+	site: 'https://yashjawale.github.io',
+	base: 'fabr',
+	integrations: [starlight({
+		title: 'Fabr',
+		description: 'Project Template Generator',
+		logo: {
+			dark: './src/assets/fabr-docs-dark.svg',
+			light: './src/assets/fabr-docs-light.svg',
+			alt: 'fabr Docs',
+			replacesTitle: true,
+		},
+		customCss: [
+			'./src/styles/starlight-theme.css',
+		],
+		expressiveCode: {
+			themes: ['github-light', 'github-dark'],
+		},
+		sidebar: [
+			{
+				label: 'General',
+				items: [
+					{ label: 'Getting Started', slug: 'docs' },
+				],
+			},
+		],
+	})],
+	vite: {
+		plugins: [tailwindcss()]
+	}
 });
